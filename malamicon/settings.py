20cc7fe4345c1fd6f14724cdd6f7910c6e741c9c
Mousty Mainer <moustymainer@gmail.com>
2022-03-19 14:33:45 +0100

    Update settings.py

--- malamicon/settings.py
+++ malamicon/settings.py
@@ -48,6 +48,9 @@ AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
@@ -131,9 +135,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
 
-STATIC_ROOT = os.path.join(BASE_DIR, )
 
 #STATIC_ROOT =
 
